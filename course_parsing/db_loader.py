4a6897b8000a0f33606a24df663ab21a4edcc6bb
Anina Hitt <ahitt@splunk.com>
2019-06-04 18:38:17 -0700

    db_loader changed a bit

--- course_parsing/db_loader.py
+++ course_parsing/db_loader.py
@@ -1,8 +1,7 @@
 import json
 import sqlite3
 
-
-# open json file, insert into db
+# open json file, play with the courses as needed
 with open('courses.json') as json_file:  
     courses = json.load(json_file)
     c = set()
@@ -14,33 +13,34 @@ with open('courses.json') as json_file:
     print(len(c))
     print(sorted(c))
 
-# # connecting to the database file
-# conn = sqlite3.connect('courses.db')
-# c = conn.cursor()
+# connecting to the database file
+conn = sqlite3.connect('courses.db')
+c = conn.cursor()
 
-# # creating a new SQLite table 
-# c.execute('DROP TABLE IF EXISTS courses');
-# c.execute('CREATE TABLE courses(key INTEGER PRIMARY KEY, department TEXT, course_num INTEGER, instr TEXT)')
+# creating a new SQLite table 
+c.execute('DROP TABLE IF EXISTS courses');
+c.execute('CREATE TABLE courses(key INTEGER PRIMARY KEY, department TEXT, course_num INTEGER, instr TEXT)')
 
-# # open json file, insert into db
-# with open('courses.json') as json_file:  
-#     courses = json.load(json_file)
-#     for course in courses:
-#     	# split course code
-#     	x = course["code"].split()
-#     	if len(x) != 2:
-#     		print("ERROR: ", x)
-#     		continue
-#     	department = x[0]
-#     	course_num = x[1]
-#     	key = course["key"]
-#     	instr = course["instr"]
+# open json file, insert into db
+with open('courses.json') as json_file:  
+    courses = json.load(json_file)
+    for course in courses:
+        # split course code
+        x = course["code"].split()
+        if len(x) != 2:
+            print("ERROR: ", x)
+            continue
+        department = x[0]
+        course_num = x[1]
+        key = course["key"]
+        instr = course["instr"]
 
-#     	# insertion
-#     	params = (key, department, course_num, instr)
-#     	c.execute("INSERT INTO courses (key, department, course_num, instr) VALUES (?, ?, ?, ?)", params)
+        # insertion
+        params = (key, department, course_num, instr)
+        c.execute("INSERT INTO courses (key, department, course_num, instr) VALUES (?, ?, ?, ?)", params)
 
 
-# # committing changes and closing the connection to the db file
-# conn.commit()
-# conn.close()
+# committing changes and closing the connection to the db file
+conn.commit()
+conn.close()
+
