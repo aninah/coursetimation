431ec3294f01dd6c0241ab8db5267431e5df5c5d
Anina Hitt <ahitt@splunk.com>
2019-08-09 09:32:50 -0700

    moving

---
+++ calculations/class_ranges.py
@@ -0,0 +1,11 @@
+import re
+
+def natural_sort(l): 
+    convert = lambda text: int(text) if text.isdigit() else text.lower() 
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+    return sorted(l, key = alphanum_key)
+
+def comparison(code1, code2):
+	return code1 > code2
+
+print(comparison("0090F", "0090G"))
